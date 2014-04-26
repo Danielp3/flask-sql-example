c24ee25d66468b745aa027bed10fd0918c651d00
Daniel <danielpeskenazi@yahoo.com>
2014-04-26 18:45:30 +0000

    make front page display names

--- example_app/__init__.py
+++ example_app/__init__.py
@@ -1,8 +1,15 @@
 from flask import Flask, render_template
+from flaskext.mysql import MySQL
 
 app = Flask(__name__)
 app.config.from_object('settings')
 
+mysql = MySQL()
+mysql.init_app(app)
+
 @app.route('/')
 def index():
-    return render_template('index.html')
+    cursor = mysql.get_db().cursor()
+    cursor.execute('select * from people')
+    people = cursor.fetchall()
+    return render_template('index.html', people=people)
