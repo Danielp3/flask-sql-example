840261fa0bb1383f48564ddb54f2b858980df61f
Daniel <danielpeskenazi@yahoo.com>
2014-04-26 18:45:05 +0000

    allow running on different port

--- runapp.py
+++ runapp.py
@@ -1,4 +1,8 @@
 from example_app import app
+import sys
 
 if __name__ == '__main__':
-    app.run()
+    if len(sys.argv) < 2:
+        app.run(host='0.0.0.0')
+    else:
+        app.run(host='0.0.0.0', port=int(sys.argv[1]))
